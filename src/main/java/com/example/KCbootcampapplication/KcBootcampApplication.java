87690096773fc8ff712146328d8ceaa205b02f37
Krisjanis Alliks <krisjanisalliks@gmail.com>
2021-06-21 14:33:40 +0300

    Initial commit.

---
+++ src/main/java/com/example/KCbootcampapplication/KcBootcampApplication.java
@@ -0,0 +1,13 @@
+package com.example.KCbootcampapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KcBootcampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KcBootcampApplication.class, args);
+	}
+
+}
