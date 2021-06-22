685cb59d579f41255eed7c48e6af1e9c02063e96
Lidia Belyaeva <lvcaciag@gmail.com>
2021-06-22 14:28:28 +0300

    Database dump file added, connection extablished

--- src/main/java/domain/TestAnswer.java
+++ src/main/java/domain/TestAnswer.java
@@ -4,11 +4,20 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name="answers")
 public class TestAnswer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "answer_id")
     private int id;
+    @Column(name = "answer_text")
     private String answers; // "A B C D" ANSWERS.SPLIT(STRING)
+    @Column(name = "answer_correct")
     private String correctAnswer;
 }
--- src/main/java/domain/TestQuestion.java
+++ src/main/java/domain/TestQuestion.java
@@ -4,12 +4,23 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class TestQuestion {
-    private int id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "question_id")
+    private Integer id;
+    @Column(name = "question_body")
     private String question;
+    @Column(name = "question_variants")
     private String questionAnswerVariants;
+    @Column(name = "question_type")
     private String type;
 }
