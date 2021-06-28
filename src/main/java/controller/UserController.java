5ee331ef07b009cd96b5c05297b0e78a64028af2
Lidia Belyaeva <lvcaciag@gmail.com>
2021-06-28 18:23:00 +0300

    LoginController added; package name changed to TemplateS to be recognized by Thymeleaf

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -1,6 +1,7 @@
 package controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -15,4 +16,8 @@ public class UserController {
 
     }
      */
+    @GetMapping("/home")
+    public String getHomePage(){
+        return "welcome";
+    }
 }
