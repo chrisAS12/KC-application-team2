03d09d7c0802e55b819c6b0deb7981221d382f96
Edijs Elijs Skroders <edijsskroders@gmail.com>
2021-07-01 12:51:58 +0300

    still db tests

--- src/test/java/com/example/KCbootcampapplication/DatabaseTests.java
+++ src/test/java/com/example/KCbootcampapplication/DatabaseTests.java
@@ -4,12 +4,18 @@ import com.example.KCbootcampapplication.domain.KnowledgeCheck;
 import com.example.KCbootcampapplication.domain.TestQuestion;
 import com.example.KCbootcampapplication.domain.User;
 import com.example.KCbootcampapplication.service.DatabaseManager;
-import org.junit.Assert;
-import org.junit.jupiter.api.Test;
 import org.springframework.test.context.ContextConfiguration;
+
+import java.nio.charset.Charset;
+import java.sql.SQLException;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 
 @ContextConfiguration(locations = "classpath:hibernate.cfg.xml")
@@ -22,22 +28,22 @@ public class DatabaseTests {
     TestQuestion tq;
 
     @Test
-    void test00_insertUser(){
+    public void test00_insertUser(){
         try {
             u.setEmail("test123@testtest.test");
             u.setPassword("test");
             u.setRole("student");
             u.setLogin("test123");
-            Assert.assertEquals("User info not set correctly","email: test123@testtest.test, role: student", "email: " + u.getEmail() + ", role: " + u.getRole());
+            assertEquals( "User info not set correctly","email: test123@testtest.test, role: student", "email: " + u.getEmail() + ", role: " + u.getRole());
             dm.insertUser(u.getEmail(),u.getPassword(), u.getLogin(), u.getRole());
-            System.out.println("User created" +  u.getId());
+            System.out.println("User created" + u.getLogin());
         }catch (Exception e){
             e.printStackTrace();
         }
     }
 
     @Test
-    void test02_DBMLogin(){
+    public void test02_DBMLogin(){
         try {
             u.setEmail("test123@testtest.test");
             u.setPassword("test");
@@ -51,7 +57,7 @@ public class DatabaseTests {
     }
 
     @Test
-    void test03_DBMSave(){
+    public void test03_DBMSave(){
         tq = new TestQuestion();
         kc = new KnowledgeCheck();
         testQuestions = new ArrayList<>();
@@ -78,25 +84,43 @@ public class DatabaseTests {
     }
 
     @Test
-    void test04_DBMSaveQuestions(){
+    public void test04_DBMSaveQuestions(){
         testQuestions = new ArrayList<>();
         tq = new TestQuestion();
-        testQuestions.add(tq);
-        dm.saveQuestions(testQuestions);
+        tq.setQuestion("what");
+        Random a = new Random();
+        byte[] letters = new byte[7];
+        int b = a.nextInt(30);
+        try {
+            while (b + 1 > 1) {
+                String testStr = new String(letters, Charset.forName("UTF-8"));
+                tq.setQuestion(testStr);
+                kc.
+                tq.setType("a");
+                tq.setCorrectAnswer("a" + b);
+                testQuestions.add(tq);
+                System.out.println(tq.getQuestion() + " " + tq.getCorrectAnswer() + " " + tq.getClass());
+                b--;
+            }
+            dm.saveQuestions(testQuestions);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
     }
 
     @Test
-    void test05_DBMUpdate(){
+    public void test05_DBMUpdate(){
         //dm.update();
     }
 
     @Test
-    void test06_DBMUpdateQuestions(){
+    public void test06_DBMUpdateQuestions(){
         //dm.updateQuestion();
     }
 
-    @Test
-    void test07DBMDelete(){
+    //@Test
+    public void test07DBMDelete(){
         kc = new KnowledgeCheck();
         testQuestions = new ArrayList<>();
         kc.setKcName("Test");
@@ -105,7 +129,12 @@ public class DatabaseTests {
         kc.setIsShow(true);
         kc.setStartTime(LocalDateTime.now());
         kc.setFinishTime(LocalDateTime.now());
-        dm.delete(u);
-        dm.delete(kc);
+        try {
+            dm.delete(u);
+            dm.delete(kc);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
     }
 }
