6cb79a2cfd59dcb2f4d8dffb7ff20ff673c037c7
Edijs Elijs Skroders <edijsskroders@gmail.com>
2021-07-06 11:36:31 +0300

    Selenium admin tests

--- src/test/java/com/example/KCbootcampapplication/AdminSelenium.java
+++ src/test/java/com/example/KCbootcampapplication/AdminSelenium.java
@@ -1,6 +1,5 @@
 package com.example.KCbootcampapplication;
 
-import org.hibernate.mapping.Value;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.FixMethodOrder;
@@ -8,13 +7,10 @@ import org.junit.Test;
 import org.junit.runners.MethodSorters;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.Assert.*;
-
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
@@ -52,12 +48,12 @@ public class AdminSelenium {
         driver.get(url + "login");
         driver.findElement(By.name("email")).sendKeys("admin@test.com");
         driver.findElement(By.name("pwd")).sendKeys("test123");
+        sleep();
         driver.findElement(By.cssSelector("button[type=\"submit\"]")).click();
         sleep();
-        assertEquals("Didnt get transfered to tests","http://localhost:8080/admin/dashboard",driver.getCurrentUrl());
+        assertEquals("Didnt get transfered to dashboard","http://localhost:8080/admin/dashboard",driver.getCurrentUrl());
     }
 
-    //getting whitelabel
     @Test
     public void test02createNewKC(){
         driver.findElement(By.xpath("//input[@value=\"Create new test\"]")).click();
@@ -66,23 +62,44 @@ public class AdminSelenium {
         assertTrue(driver.getTitle().contains("Create"));
         driver.findElement(By.name("name")).sendKeys("test KC");
         driver.findElement(By.name("isShow")).click();
+        sleep();
         driver.findElement(By.cssSelector("button[type=\"submit\"]")).click();
         sleep();
-        assertTrue(driver.getCurrentUrl().contains("dashboard"));
     }
 
-    //WIP
     @Test
-    public void test03CreateUser(){
+    public void test03CreateNewQuestion(){
+        driver.findElement(By.xpath("//input[@value=\"Add multiple choice question\"]")).click();
+        driver.findElement(By.name("title")).sendKeys("Is it worth using Selenium?");
+        driver.findElement(By.name("answer")).sendKeys("a");
+        driver.findElement(By.name("opt1")).sendKeys("yes");
+        driver.findElement(By.name("opt2")).sendKeys("no");
+        sleep();
+        driver.findElement(By.cssSelector("button[type=\"submit\"]")).click();
+        sleep();
+    }
+
+    @Test
+    public void test04CreateUser(){
         driver.get(url + "admin/dashboard");
         sleep();
         driver.findElement(By.xpath("//input[@value=\"Create new user\"]")).click();
         sleep();
+        driver.findElement(By.name("name")).sendKeys("test KC Sel");
+        driver.findElement(By.name("email")).sendKeys("test123456@test.com ");
+        driver.findElement(By.name("login")).sendKeys("test123");
+        driver.findElement(By.name("password")).sendKeys("test123");
+        driver.findElement(By.xpath("//input[@value=\"student\"]")).click();
+        sleep();
+        driver.findElement(By.cssSelector("button[type=\"submit\"]")).click();
+        sleep();
+        assertEquals("Didnt get transfered to tests","http://localhost:8080/admin/dashboard",driver.getCurrentUrl());
+
     }
 
     //WIP
     @Test
-    public void test04CheckResults(){
+    public void test05CheckResults(){
         driver.get(url + "admin/dashboard");
         sleep();
         driver.findElement(By.xpath("//input[@value=\"Check results\"]")).click();
